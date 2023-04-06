61a30b68dc40c0e0bf41aea00a5ca356cd367d6b
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-04-06 09:58:13 -0500

    Agregar validación de existencia de cuenta por parámetros

--- backend/pruebatecnica/src/main/java/com/neoris/tst/pruebatecnica/repository/CuentaRepository.java
+++ backend/pruebatecnica/src/main/java/com/neoris/tst/pruebatecnica/repository/CuentaRepository.java
@@ -4,5 +4,6 @@ import com.neoris.tst.pruebatecnica.domain.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CuentaRepository extends JpaRepository<Cuenta, Integer> {
-    boolean existsByNumeroCuentaAndEstadoAndTipoCuentaId(String numeroCuenta, boolean estado, Integer tipoCuentaId);
+    boolean existsByNumeroCuentaAndClienteIdAndTipoCuentaIdAndEstado
+            (String numeroCuenta, Integer clienteId, Integer tipoCuentaId, Boolean estado);
 }
