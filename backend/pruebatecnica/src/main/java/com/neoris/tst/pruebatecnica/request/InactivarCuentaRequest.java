a6adb7285aaa3f8dcd16be2e637cf5d0f7ee737e
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-04-08 12:53:16 -0500

    Objetos request y response para inactivar cuenta

--- backend/pruebatecnica/src/main/java/com/neoris/tst/pruebatecnica/request/InactivarCuentaRequest.java
+++ backend/pruebatecnica/src/main/java/com/neoris/tst/pruebatecnica/request/InactivarCuentaRequest.java
@@ -1,6 +1,8 @@
 package com.neoris.tst.pruebatecnica.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.neoris.tst.pruebatecnica.utility.validations.ActivarInactivarUsuarioValidate;
+import com.neoris.tst.pruebatecnica.utility.validations.ActivarInactivarCuentaValidate;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -14,4 +16,16 @@ import lombok.NoArgsConstructor;
 @Builder
 public class InactivarCuentaRequest {
 
+    @NotNull(message = ActivarInactivarCuentaValidate.NUMERO_CUENTA_REQUIRED)
+    @NotEmpty(message = ActivarInactivarCuentaValidate.NUMERO_CUENTA_NOT_EMPTY)
+    private String numeroCuenta;
+
+    @JsonProperty("tipo")
+    @NotNull(message = ActivarInactivarCuentaValidate.TIPO_CUENTA_DESCRIPCION_REQUIRED)
+    @NotEmpty(message = ActivarInactivarCuentaValidate.TIPO_CUENTA_DESCRIPCION_NOT_EMPTY)
+    private String tipoCuentaDescripcion;
+
+    @NotNull(message = ActivarInactivarCuentaValidate.IDENTIFICACION_NOT_NULL)
+    @NotEmpty(message = ActivarInactivarCuentaValidate.IDENTIFICACION_NOT_EMPTY)
+    String identificacion;
 }
